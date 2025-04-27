780ce66bbfe4755d4279b3b78d02221900bbccef
erfanbr <40319048+erfanbr@users.noreply.github.com>
2025-04-27 17:23:19 +0200

    [DB - Init DB & Prism Setup]

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -62,7 +62,7 @@ export default function NavBar() {
                                 <li key={link.href}>
                                     <Link href={link.href} key={link.label}
                                           className={classNames(
-                                              "flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group",
+                                              "flex items-center p-2 text-gray-900 rounded-4xl dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group",
                                               {
                                                   "dark:bg-cyan-600": isActive
                                               }
