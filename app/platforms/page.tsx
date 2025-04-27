e108e9141f86c74dab8af5afcb15fb9a6ac5dfd2
erfanbr <40319048+erfanbr@users.noreply.github.com>
2025-04-27 23:30:31 +0200

    [DB - Platforms Page] +added platform page and option to show all platforms

--- app/platforms/page.tsx
+++ app/platforms/page.tsx
@@ -1,9 +1,47 @@
 import React from "react";
+import {prisma} from "@/prisma/client";
 
-export default function PlatformPage() {
+export default async function PlatformPage() {
+    const platforms = await prisma.platform.findMany();
+
     return (
         <>
-            <h1>Platform Page</h1>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Id
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Platform name
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {platforms.map((platform) => (
+                        <>
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                    key={platform.id}>
+                                    {platform.id}
+                                </th>
+                                <td className="px-6 py-4">{platform.title}</td>
+                                <td className="px-6 py-4">
+                                    <a href="#"
+                                       className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                </td>
+                            </tr>
+                        </>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
+
         </>
     );
 }
