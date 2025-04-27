a8817d56808d353a6de129127d141ad565c14080
erfanbr <40319048+erfanbr@users.noreply.github.com>
2025-04-28 00:52:36 +0200

    [DB - Platform details Page] +added base platform detaisl page

---
+++ app/platforms/[slug]/page.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import {prisma} from "@/prisma/client";
+
+interface Props {
+    params: { slug: string }
+}
+
+export default async function PlatformEditPage(myProp: Props) {
+    const platform = await prisma.platform.findUnique({
+        where: {id: parseInt(myProp.params.slug)}
+    })
+
+    return (
+        <>
+            <div>
+                {/*<h1>Post: {myProp.params.slug}</h1>*/}
+                {/*<div>{platform!.title}</div>*/}
+                {/*<div>{platform!.id}</div>*/}
+
+
+                {/*<div className="flex justify-center m-5">*/}
+                {/*    <button id="defaultModalButton" data-modal-target="defaultModal" data-modal-toggle="defaultModal" className="block text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" type="button">*/}
+                {/*        Create product*/}
+                {/*    </button>*/}
+                {/*</div>*/}
+
+                <div className="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                    {/*// <!-- Modal header -->*/}
+                    <div
+                        className="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                            Edit Platform
+                        </h3>
+                        <button type="button"
+                                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                data-modal-toggle="defaultModal">
+                            <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd"
+                                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                      clipRule="evenodd"></path>
+                            </svg>
+                            <span className="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    {/*// <!-- Modal body -->*/}
+                    <form action="#">
+                        <div className="grid gap-4 mb-4 sm:grid-cols-2">
+                            <div>
+                                <label htmlFor="id"
+                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ID:</label>
+                                <input type="text" name="id" id="id"
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-zinc-700 dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                       placeholder={platform!.id.toString()} disabled={true}/>
+                            </div>
+                            <div>
+                                <label htmlFor="title"
+                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title:</label>
+                                <input type="text" name="title" id="title"
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-100 dark:placeholder-gray-400 dark:text-zinc-700 dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                       placeholder="Product brand" required={true} value={platform!.title}/>
+                            </div>
+
+
+                        </div>
+
+                        {/*TODO: Fix button style*/}
+                        <div className="text-right">
+                            <button className="p-2 mr-3 bg-amber-700 rounded-2xl border-2 border-amber-50">Delete</button>
+                            <button className="p-2 bg-amber-700 rounded-2xl border-2 border-amber-50">Update Platform</button>
+                            <button type="submit"
+                                    className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4
+                                focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
+                                dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                <svg className="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd"
+                                          d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                          clipRule="evenodd"></path>
+                                </svg>
+                                Add new product
+                            </button>
+                        </div>
+
+                    </form>
+                </div>
+
+
+                {/*<div id="defaultModal" tabIndex="-1" aria-hidden="true"*/}
+                {/*     className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">*/}
+                {/*    <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">*/}
+                {/*        <!-- Modal content -->*/}
+
+                {/*    </div>*/}
+                {/*</div>*/}
+            </div>
+        </>
+    );
+}
