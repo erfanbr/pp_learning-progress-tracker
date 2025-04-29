4746d2dbff239cce308014f7d0bc30fc14971afd
erfanbr <40319048+erfanbr@users.noreply.github.com>
2025-04-29 23:34:51 +0200

    [ Platform - Sorting Icon] + added small sorting icon

--- app/platforms/PlatformTable.tsx
+++ app/platforms/PlatformTable.tsx
@@ -1,34 +1,31 @@
-import React, {useState} from "react";
+import React from "react";
 import {prisma} from "@/prisma/client";
 import Link from "next/link";
-import {sort} from "fast-sort"
-import {FaCaretUp, FaCaretDown} from "react-icons/fa";
+import {sort} from "fast-sort";
+import {FaCaretDown, FaCaretUp} from "react-icons/fa";
 
 interface Props {
-    sortBy: string;
-    sortType: string;
+    sortBy: string,
+    sortType: string
 }
 
-type Category = {
+type Platform = {
     id: number;
     title: string;
 };
 
+export default async function PlatformTable({sortBy, sortType} :Props) {
+    const platforms = await prisma.platform.findMany();
 
-export default async function CategoryTable({sortBy, sortType}: Props) {
-
-
-    const categories = await prisma.catergory.findMany();
     const sortMethod = sortType;
 
-    const sortedCategories = sortMethod === "asc"
-        ? sort(categories).asc(category =>
-            sortBy in category ? category[sortBy as keyof Category] : category.id
+    const sortedPlatforms = sortMethod === "asc"
+        ? sort(platforms).asc(platform =>
+            sortBy in platform ? platform[sortBy as keyof Platform] : platform.id
         )
-        : sort(categories).desc(category =>
-            sortBy in category ? category[sortBy as keyof Category] : category.id
+        : sort(platforms).desc(platform =>
+            sortBy in platform ? platform[sortBy as keyof Platform] : platform.id
         );
-
 
     return (
         <>
@@ -38,7 +35,7 @@ export default async function CategoryTable({sortBy, sortType}: Props) {
                     <tr>
                         <th scope="col" className="px-6 py-3">
                             <div className="flex"><Link
-                                href={`/categories?sortBy=id&sortType=${sortMethod === "asc" ? "desc" : "asc"}`}>
+                                href={`/platforms?sortBy=id&sortType=${sortMethod === "asc" ? "desc" : "asc"}`}>
                                 ID
                             </Link>
                                 {sortBy === "id" ? (
@@ -52,8 +49,8 @@ export default async function CategoryTable({sortBy, sortType}: Props) {
                         <th scope="col" className="px-6 py-3">
                             <div className="flex">
                                 <Link
-                                    href={`/categories?sortBy=title&sortType=${sortMethod === "asc" ? "desc" : "asc"}`}>
-                                    Category name
+                                    href={`/platforms?sortBy=title&sortType=${sortMethod === "asc" ? "desc" : "asc"}`}>
+                                    Platform name
                                 </Link>
                                 {sortBy === "title" ? (
                                     sortMethod === "asc" ? (
@@ -70,16 +67,15 @@ export default async function CategoryTable({sortBy, sortType}: Props) {
                     </tr>
                     </thead>
                     <tbody>
-                    {sortedCategories.map((category) => (
+                    {sortedPlatforms.map((platform) => (
 
-                        <tr key={category.id}
-                            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <tr key={platform.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
                             <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {category.id}
+                                {platform.id}
                             </th>
-                            <td className="px-6 py-4">{category.title}</td>
+                            <td className="px-6 py-4">{platform.title}</td>
                             <td className="px-6 py-4">
-                                <Link href={`/categories/${category.id}`}
+                                <Link href={`/platforms/${platform.id}`}
                                       className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
                             </td>
                         </tr>
--- app/platforms/page.tsx
+++ app/platforms/page.tsx
@@ -1,46 +1,23 @@
 import React from "react";
 import {prisma} from "@/prisma/client";
 import Link from "next/link";
+import PlatformTable from "@/app/platforms/PlatformTable";
 
-export default async function PlatformPage() {
-    const platforms = await prisma.platform.findMany();
+interface Props {
+    searchParams: {
+        sortBy?: string,
+        sortType?: string
+    };
+}
+
+export default async function PlatformPage( {searchParams}: Props) {
+    const sortBy = searchParams?.sortBy || "id";
+    const sortType = searchParams?.sortType || "asc";
+
 
     return (
         <>
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            Id
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Platform name
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Action
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {platforms.map((platform) => (
-
-                            <tr key={platform.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {platform.id}
-                                </th>
-                                <td className="px-6 py-4">{platform.title}</td>
-                                <td className="px-6 py-4">
-                                    <Link href={`/platforms/${platform.id}`}
-                                       className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-                                </td>
-                            </tr>
-
-                    ))}
-                    </tbody>
-                </table>
-            </div>
-
+            <PlatformTable sortBy={sortBy} sortType={sortType}></PlatformTable>
 
         </>
     );
