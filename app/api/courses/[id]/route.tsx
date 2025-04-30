daa65de30f132d4bea4230314ccbe76a81c3d50b
erfanbr <40319048+erfanbr@users.noreply.github.com>
2025-04-30 22:51:35 +0200

    [ API - POST Course ]

---
+++ app/api/courses/[id]/route.tsx
@@ -0,0 +1,13 @@
+import {NextRequest, NextResponse} from "next/server";
+import {prisma} from "@/prisma/client";
+
+interface Props {
+    params: { id: string }
+}
+
+export async function GET(request: NextRequest, {params}: Props) {
+    const course = await prisma.course.findUnique({where: {id: parseInt(params.id)}});
+    if (!course) return NextResponse.json({error: "Course not found"}, {status: 404})
+
+    return NextResponse.json(course);
+}
