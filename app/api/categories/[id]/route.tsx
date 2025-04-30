17f9455a37508f351b633aa20e487b8373b2ec40
erfanbr <40319048+erfanbr@users.noreply.github.com>
2025-04-30 22:49:16 +0200

    [ API - POST Category ]

---
+++ app/api/categories/[id]/route.tsx
@@ -0,0 +1,13 @@
+import {NextRequest, NextResponse} from "next/server";
+import {prisma} from "@/prisma/client";
+
+interface Props {
+    params: { id: string }
+}
+
+export async function GET(request: NextRequest, {params}: Props) {
+    const categories = await prisma.catergory.findUnique({where: {id: parseInt(params.id)}});
+    if (!categories) return NextResponse.json({error: "Category not found"}, {status: 404})
+
+    return NextResponse.json(categories);
+}
