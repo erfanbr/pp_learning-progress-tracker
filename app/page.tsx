a78240bda5700cfa09cdd0a1d1ca1e7cddf8dff5
erfanbr <40319048+erfanbr@users.noreply.github.com>
2025-05-06 20:40:40 +0200

    [UI - Added global color customization override]

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
 export default function Home() {
     return (
-        <div>Main Part</div>
-    );
+        <>
+            <div>Main Part</div>
+            <div>
+                <button type="button" className="btn btn-primary">
+                    Custom Primary Button
+                </button>
+                <button type="submit"
+                        className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4
+                        focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
+                        dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                    Add new product
+                </button>
+                <button type="submit"
+                        className=" m-1.5 text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4
+                        focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
+                        dark:bg-erfan-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                    Erfan's button
+                </button>
+            </div>
+
+        </>
+    )
+        ;
 }
--- app/platforms/[slug]/page.tsx
+++ app/platforms/[slug]/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import {prisma} from "@/prisma/client";
 import Link from "next/link";
+import {Button} from "flowbite-react";
+import {theme} from "flowbite-react/plugin/tailwindcss/theme";
+import {FaTrashCan} from "react-icons/fa6";
+import { FaSave } from "react-icons/fa";
+
 
 interface Props {
     params: { slug: string }
@@ -32,14 +37,15 @@ export default async function PlatformEditPage(myProp: Props) {
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                             Edit Platform
                         </h3>
-                        <Link className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                              href="/platforms/">
-                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd"
-                                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                          clipRule="evenodd"></path>
-                                </svg>
+                        <Link
+                            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                            href="/platforms/">
+                            <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd"
+                                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                      clipRule="evenodd"></path>
+                            </svg>
                         </Link>
                     </div>
                     {/*// <!-- Modal body -->*/}
@@ -50,7 +56,8 @@ export default async function PlatformEditPage(myProp: Props) {
                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ID:</label>
                                 <input type="text" name="id" id="id"
                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-zinc-400 dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                       placeholder="id goes here" disabled={true} readOnly={true} defaultValue={platform!.id}/>
+                                       placeholder="id goes here" disabled={true} readOnly={true}
+                                       defaultValue={platform!.id}/>
                             </div>
                             <div>
                                 <label htmlFor="title"
@@ -64,21 +71,37 @@ export default async function PlatformEditPage(myProp: Props) {
                         </div>
 
                         {/*TODO: Fix button style*/}
+                        {/*<div className="inline-flex rounded-md shadow-xs" role="group">*/}
+                        {/*    <Button color="primary" className="rounded-2xl mx-2 text-right">Update</Button>*/}
+                        {/*    <Button color="delete" className="rounded-2xl mx-2 text-right">Small</Button>*/}
+                        {/*</div>*/}
                         <div className="text-right">
-                            <button className="p-2 mr-3 bg-amber-700 rounded-2xl border-2 border-amber-50">Delete</button>
-                            <button className="p-2 bg-amber-700 rounded-2xl border-2 border-amber-50">Update Platform</button>
+                            {/*<button className="p-2 mr-3 bg-amber-800 rounded-2xl border-2 border-amber-50">Delete</button>*/}
+                            <button type="button"
+                                    className="mx-4 text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
+                                <FaTrashCan className="mr-1.5 -ml-1.5 w-5 h-5"/>
+                                Delete
+                            </button>
                             <button type="submit"
-                                    className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4
-                                focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
-                                dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                    className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+
+                                <FaSave className="mr-1.5 -ml-1.5 w-5 h-5"/>
+                                Save changes
+                            </button>
+                            <button type="submit"
+                                    className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800
+                                    focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm
+                                    px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                                 <svg className="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
                                      xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd"
+                                    <path fill-rule="evenodd"
                                           d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                          clipRule="evenodd"></path>
+                                          clip-rule="evenodd"></path>
                                 </svg>
                                 Add new product
                             </button>
+                            <p className="primary">This is test text</p>
+
                         </div>
 
                     </form>
