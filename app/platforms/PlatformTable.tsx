fc13e8acdb33f4f557526231fd793ce69cddc047
erfanbr <40319048+erfanbr@users.noreply.github.com>
2025-05-14 20:46:11 +0200

    [UI Improvement - Added header to platforms page]

--- app/platforms/PlatformTable.tsx
+++ app/platforms/PlatformTable.tsx
@@ -3,7 +3,6 @@ import {prisma} from "@/prisma/client";
 import Link from "next/link";
 import {sort} from "fast-sort";
 import {FaCaretDown, FaCaretUp, FaSave} from "react-icons/fa";
-import AddItemButton from "@/app/components/AddItemButton";
 import CustomButton from "@/app/components/CustomButton";
 
 interface Props {
@@ -31,63 +30,73 @@ export default async function PlatformTable({sortBy, sortType}: Props) {
 
     return (
         <>
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            <div className="flex"><Link
-                                href={`/platforms?sortBy=id&sortType=${sortMethod === "asc" ? "desc" : "asc"}`}>
-                                ID
-                            </Link>
-                                {sortBy === "id" ? (
-                                    sortMethod === "asc" ? (
-                                        <FaCaretDown className="ml-3"/>
-                                    ) : (
-                                        <FaCaretUp className="ml-3"/>
-                                    )
-                                ) : null}</div>
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            <div className="flex">
-                                <Link
-                                    href={`/platforms?sortBy=title&sortType=${sortMethod === "asc" ? "desc" : "asc"}`}>
-                                    Platform name
+            <div className="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <div
+                        className="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                            Platforms
+                        </h3>
+                    </div>
+                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead
+                            className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                <div className="flex"><Link
+                                    href={`/platforms?sortBy=id&sortType=${sortMethod === "asc" ? "desc" : "asc"}`}>
+                                    ID
                                 </Link>
-                                {sortBy === "title" ? (
-                                    sortMethod === "asc" ? (
-                                        <FaCaretDown className="ml-3"/>
-                                    ) : (
-                                        <FaCaretUp className="ml-3"/>
-                                    )
-                                ) : null}
-                            </div>
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Action
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {sortedPlatforms.map((platform) => (
-
-                        <tr key={platform.id}
-                            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {platform.id}
+                                    {sortBy === "id" ? (
+                                        sortMethod === "asc" ? (
+                                            <FaCaretDown className="ml-3"/>
+                                        ) : (
+                                            <FaCaretUp className="ml-3"/>
+                                        )
+                                    ) : null}</div>
                             </th>
-                            <td className="px-6 py-4">{platform.title}</td>
-                            <td className="px-6 py-4">
-                                <Link href={`/platforms/${platform.id}`}
-                                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-                            </td>
+                            <th scope="col" className="px-6 py-3">
+                                <div className="flex">
+                                    <Link
+                                        href={`/platforms?sortBy=title&sortType=${sortMethod === "asc" ? "desc" : "asc"}`}>
+                                        Platform name
+                                    </Link>
+                                    {sortBy === "title" ? (
+                                        sortMethod === "asc" ? (
+                                            <FaCaretDown className="ml-3"/>
+                                        ) : (
+                                            <FaCaretUp className="ml-3"/>
+                                        )
+                                    ) : null}
+                                </div>
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Action
+                            </th>
                         </tr>
+                        </thead>
+                        <tbody>
+                        {sortedPlatforms.map((platform) => (
 
-                    ))}
-                    </tbody>
-                </table>
-                <div className="text-right py-5">
-                    <CustomButton href="/platforms/new" icon={FaSave} buttonType={"primary"}>Add Platform</CustomButton>
+                            <tr key={platform.id}
+                                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {platform.id}
+                                </th>
+                                <td className="px-6 py-4">{platform.title}</td>
+                                <td className="px-6 py-4">
+                                    <Link href={`/platforms/${platform.id}`}
+                                          className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
+                                </td>
+                            </tr>
+
+                        ))}
+                        </tbody>
+                    </table>
+                    <div className="text-right py-5">
+                        <CustomButton href="/platforms/new" icon={FaSave} buttonType={"primary"}>Add
+                            Platform</CustomButton>
+                    </div>
                 </div>
             </div>
         </>
