f56154a1a6664f5dd23aa57ef7239dd6098b77fa
erfanbr <40319048+erfanbr@users.noreply.github.com>
2025-06-09 13:46:21 +0200

    [Learning Path - Details Page] * minor UI improvement

--- app/learning_paths/[slug]/LearningPathsEditPageForm.tsx
+++ app/learning_paths/[slug]/LearningPathsEditPageForm.tsx
@@ -114,8 +114,8 @@ export default function LearningPathsEditPageForm({learningPathsCoursesData}: Pr
                                     {learningPathsCoursesData.map((c) => (
                                         <li className="mb-10 ms-6" key={c.id}>
                                         <span
-                                            className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-2 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                                            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true"
+                                            className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-2 ring-white dark:ring-gray-100 dark:bg-primary-500">
+                                            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-gray-100" aria-hidden="true"
                                                  xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                                 <path
                                                     d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
