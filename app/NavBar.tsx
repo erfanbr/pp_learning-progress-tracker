9982f77e37f2459fd7e91459cb4b93b8e770a53a
erfanbr <40319048+erfanbr@users.noreply.github.com>
2025-07-02 23:33:35 +0200

    [Nav Bar - Minor modification]

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,14 +1,21 @@
 'use client';
 import React, {useState} from "react";
 import Link from "next/link";
-import { useRouter } from 'next/router';
-import {GiBookCover, GiPlatform } from "react-icons/gi";
-import {MdLibraryBooks, MdSpaceDashboard, MdEvent, MdSpeakerNotes, MdSettingsApplications, MdCategory} from "react-icons/md";
+import {useRouter} from 'next/router';
+import {GiBookCover, GiPlatform} from "react-icons/gi";
+import {
+    MdLibraryBooks,
+    MdSpaceDashboard,
+    MdEvent,
+    MdSpeakerNotes,
+    MdSettingsApplications,
+    MdCategory
+} from "react-icons/md";
 import {CgPerformance} from "react-icons/cg";
-import { IoHardwareChip } from "react-icons/io5";
+import {IoHardwareChip} from "react-icons/io5";
 import {usePathname} from "next/navigation";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { MdClose } from "react-icons/md";
+import {RxHamburgerMenu} from "react-icons/rx";
+import {MdClose} from "react-icons/md";
 import {IconType} from "react-icons";
 import classNames from "classnames";
 import navBarLinks from "@/app/components/NavBar/NavBarLinks";
@@ -78,33 +85,36 @@ export default function NavBar() {
                             const Icon: IconType = link.icon;
 
                             return (
-                                <li key={link.href}>
-                                    <Link onClick={() => setIsSidebarOpen(false)} href={link.href} key={link.label}
-                                          className={classNames(
-                                              "flex items-center p-2 text-gray-900 rounded-4xl dark:text-white hover:bg-gray-100 dark:hover:bg-primary-500 group",
-                                              {
-                                                  "dark:bg-primary-500": isActive,
-                                                  "border-2 border-zinc-200": isActive
-                                              }
-                                          )}>
-                                        <Icon
-                                            className={classNames(
-                                                "w-8 h-8 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white",
-                                                {
-                                                    "dark:text-zinc-100" : isActive,
-                                                    "dark:text-gray-400" : !isActive
-                                                }
-                                            )}/>
+                                <>
+                                    <li key={link.href}>
+                                        <Link onClick={() => setIsSidebarOpen(false)} href={link.href} key={link.label}
+                                              className={classNames(
+                                                  "flex items-center p-2 text-gray-900 rounded-4xl dark:text-white hover:bg-gray-100 dark:hover:bg-primary-500 group",
+                                                  {
+                                                      "dark:bg-primary-500": isActive,
+                                                      "border-2 border-zinc-200": isActive
+                                                  }
+                                              )}>
+                                            <Icon
+                                                className={classNames(
+                                                    "w-8 h-8 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white",
+                                                    {
+                                                        "dark:text-zinc-100": isActive,
+                                                        "dark:text-gray-400": !isActive
+                                                    }
+                                                )}/>
 
-                                        <span
-                                            className={classNames(
-                                                'ml-3',
-                                                {
-                                                    "text-zinc-100": isActive
-                                                })}>{link.label}</span>
-                                    </Link>
-                                </li>
+                                            <span
+                                                className={classNames(
+                                                    'ml-3',
+                                                    {
+                                                        "text-zinc-100": isActive
+                                                    })}>{link.label}</span>
+                                        </Link>
+                                    </li>
+                                </>
                             );
+
                         })}
                     </ul>
                 </div>
--- app/components/NavBar/NavBarLinks.tsx
+++ app/components/NavBar/NavBarLinks.tsx
@@ -1,17 +1,25 @@
-import {GiBookCover, GiPlatform } from "react-icons/gi";
-import {MdLibraryBooks, MdSpaceDashboard, MdEvent, MdSpeakerNotes, MdSettingsApplications, MdCategory} from "react-icons/md";
+import {GiBookCover, GiPlatform} from "react-icons/gi";
+import {
+    MdLibraryBooks,
+    MdSpaceDashboard,
+    MdEvent,
+    MdSpeakerNotes,
+    MdSettingsApplications,
+    MdCategory
+} from "react-icons/md";
 import {CgPerformance} from "react-icons/cg";
-import { IoHardwareChip } from "react-icons/io5";
-import { GiPathDistance } from "react-icons/gi";
+import {IoHardwareChip} from "react-icons/io5";
+import {GiPathDistance} from "react-icons/gi";
 
 
 const navBarLinks = [
     {label: "Overview", href: "/", icon: MdSpaceDashboard},
     {label: "Learning Paths", href: "/learning_paths", icon: GiPathDistance},
     {label: "Courses", href: "/courses", icon: MdLibraryBooks},
-    {label: "Platforms", href: "/platforms", icon: GiPlatform},
-    {label: "Categories", href: "/categories", icon: MdCategory},
-    {label: "Technologies", href: "/technologies", icon: IoHardwareChip},
+    // {label: "Courses Overview", href: "/courses", icon: MdLibraryBooks, parentCategory: "courses"},
+    {label: "Platforms", href: "/platforms", icon: GiPlatform, parentCategory: "courses"},
+    {label: "Categories", href: "/categories", icon: MdCategory, parentCategory: "courses"},
+    {label: "Technologies", href: "/technologies", icon: IoHardwareChip, parentCategory: "courses"},
     // {label: "Schedule", href: "/schedule", icon: MdEvent},
     // {label: "Performance", href: "/performance", icon: CgPerformance},
     // {label: "Notes", href: "/notes", icon: MdSpeakerNotes},
