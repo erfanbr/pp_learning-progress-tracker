bbcff406b7303a2f6ac7508ad94fbd4a1a370e23
erfanbr <40319048+erfanbr@users.noreply.github.com>
2025-07-28 00:26:31 +0200

    [NavBar - UserInfo] + Added Skeleton

--- app/AuthStatus.tsx
+++ app/AuthStatus.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import classNames from "classnames";
 import {RxAvatar} from "react-icons/rx";
 import {Session} from 'next-auth';
+import {Skeleton} from "@/app/components";
 
 interface Props {
     status: 'authenticated' | 'unauthenticated' | 'loading';
@@ -13,7 +14,26 @@ interface Props {
 const AuthStatus: React.FC<Props> = ({status, session}) => {
     // const {status, data: session} = useSession();
 
-    if (status === "loading") return null;
+    if (status === "loading") return (
+        <>
+            <div className="pt-4 pb-8 ml-1 animate-pulse">
+                <div className="flex items-center space-x-4">
+                    <div className="w-12 h-12 bg-gray-100 rounded-full">
+                    </div>
+                    <div className="flex flex-col space-y-2">
+                        <div className="flex items-center space-x-2">
+                            <div className="w-24 h-4 bg-gray-700 rounded">
+                                <Skeleton />
+                            </div>
+                        </div>
+                        <div className="w-16 h-3 bg-gray-700 rounded">
+                            <Skeleton />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
     if (status === "unauthenticated")
         return (
             <>
