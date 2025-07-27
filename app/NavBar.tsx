838868a92519d944016321f23cbf25229cb7cb10
erfanbr <40319048+erfanbr@users.noreply.github.com>
2025-07-27 20:42:57 +0200

    [NavBar] * Fixed key error

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -89,36 +89,34 @@ export default function NavBar() {
                             const Icon: IconType = link.icon;
 
                             return (
-                                <>
-                                    <li key={link.href}>
-                                        <Link onClick={() => setIsSidebarOpen(false)} href={link.href} key={link.label}
-                                              className={classNames(
-                                                  "flex items-center p-2 text-gray-900 rounded-4xl dark:text-white hover:bg-gray-100 dark:hover:bg-primary-500 group",
-                                                  {
-                                                      "dark:bg-primary-500": isActive,
-                                                      "border-2 border-zinc-200": isActive
-                                                  }
-                                              )}>
-                                            <Icon
-                                                className={classNames(
-                                                    "w-8 h-8 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white",
-                                                    {
-                                                        "dark:text-zinc-100": isActive,
-                                                        "dark:text-gray-400": !isActive
-                                                    }
-                                                )}/>
-
-                                            <span
-                                                className={classNames(
-                                                    'ml-3',
-                                                    {
-                                                        "text-zinc-100": isActive
-                                                    })}>{link.label}</span>
-                                        </Link>
-                                    </li>
-                                </>
+                                <li key={link.href}>
+                                    <Link
+                                        onClick={() => setIsSidebarOpen(false)}
+                                        href={link.href}
+                                        className={classNames(
+                                            "flex items-center p-2 text-gray-900 rounded-4xl dark:text-white hover:bg-gray-100 dark:hover:bg-primary-500 group",
+                                            {
+                                                "dark:bg-primary-500": isActive,
+                                                "border-2 border-zinc-200": isActive
+                                            }
+                                        )}>
+                                        <Icon
+                                            className={classNames(
+                                                "w-8 h-8 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white",
+                                                {
+                                                    "dark:text-zinc-100": isActive,
+                                                    "dark:text-gray-400": !isActive
+                                                }
+                                            )}
+                                        />
+                                        <span className={classNames("ml-3", {
+                                            "text-zinc-100": isActive
+                                        })}>
+                    {link.label}
+                </span>
+                                    </Link>
+                                </li>
                             );
-
                         })}
 
                     </ul>
